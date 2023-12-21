8c696692cd44c63d0535b9d362ee0bb2914704ab
s1uad-dw <s1uad.dw@gmail.com>
2023-12-21 09:35:42 +0300

    save

--- src/test/java/Example.java
+++ src/test/java/Example.java
@@ -11,86 +11,86 @@ public class Example {
         assertEquals(1, 1);
     }
 
-    @Test(description = "Получение списка пользователей")
-    public void testListUsers() {
-        baseURI = "https://reqres.in/api";
-        given()
-                .param("page", 2)
-        .when()
-                .get("/users")
-                .then()
-                .body("page", equalTo(2))
-                .body("per_page", equalTo(6))
-                .body("total", equalTo(12))
-                .body("total_pages", equalTo(2))
-                .body("data.id", hasItems(7, 8, 9, 10, 11, 12))
-                .statusCode(200).log().all();
-    }
-
-    @Test(description = "Создание пользователя")
-    public void testCreateUser() {
-        baseURI = "https://reqres.in/api";
-
-        JSONObject requestParams = new JSONObject();
-        requestParams.put("name", "morpheus");
-        requestParams.put("job", "leader");
-
-        given()
-                .header("Content-Type", "application/json")
-                .body(requestParams.toJSONString())
-        .when()
-                .post("/users")
-        .then()
-                .body("name", equalTo("morpheus"))
-                .body("job", equalTo("leader"))
-                .statusCode(201).log().all();
-    }
-
-    @Test(description = "Обновление пользователя методом PUT")
-    public void testUpdateUserPUT() {
-        baseURI = "https://reqres.in/api";
-
-        JSONObject requestParams = new JSONObject();
-        requestParams.put("name", "morpheus");
-        requestParams.put("job", "zion resident");
-
-        given()
-                .header("Content-Type", "application/json")
-                .body(requestParams.toJSONString())
-        .when()
-                .put("/users/2")
-        .then()
-                .body("name", equalTo("morpheus"))
-                .body("job", equalTo("zion resident"))
-                .statusCode(200).log().all();
-    }
-
-    @Test(description = "Обновление пользователя методом PATCH")
-    public void testUpdateUserPATCH() {
-        baseURI = "https://reqres.in/api";
-
-        JSONObject requestParams = new JSONObject();
-        requestParams.put("name", "morpheus");
-        requestParams.put("job", "zion resident");
-
-        given()
-                .header("Content-Type", "application/json")
-                .body(requestParams.toJSONString())
-        .when()
-                .patch("/users/2")
-        .then()
-                .body("name", equalTo("morpheus"))
-                .body("job", equalTo("zion resident"))
-                .statusCode(200).log().all();
-    }
-
-    @Test(description = "Обновление пользователя методом PATCH")
-    public void testDeleteUser() {
-        baseURI = "https://reqres.in/api";
-
-        delete("/users/2")
-                .then()
-                .statusCode(204).log().all();
-    }
+//    @Test(description = "Получение списка пользователей")
+//    public void testListUsers() {
+//        baseURI = "https://reqres.in/api";
+//        given()
+//                .param("page", 2)
+//        .when()
+//                .get("/users")
+//                .then()
+//                .body("page", equalTo(2))
+//                .body("per_page", equalTo(6))
+//                .body("total", equalTo(12))
+//                .body("total_pages", equalTo(2))
+//                .body("data.id", hasItems(7, 8, 9, 10, 11, 12))
+//                .statusCode(200).log().all();
+//    }
+//
+//    @Test(description = "Создание пользователя")
+//    public void testCreateUser() {
+//        baseURI = "https://reqres.in/api";
+//
+//        JSONObject requestParams = new JSONObject();
+//        requestParams.put("name", "morpheus");
+//        requestParams.put("job", "leader");
+//
+//        given()
+//                .header("Content-Type", "application/json")
+//                .body(requestParams.toJSONString())
+//        .when()
+//                .post("/users")
+//        .then()
+//                .body("name", equalTo("morpheus"))
+//                .body("job", equalTo("leader"))
+//                .statusCode(201).log().all();
+//    }
+//
+//    @Test(description = "Обновление пользователя методом PUT")
+//    public void testUpdateUserPUT() {
+//        baseURI = "https://reqres.in/api";
+//
+//        JSONObject requestParams = new JSONObject();
+//        requestParams.put("name", "morpheus");
+//        requestParams.put("job", "zion resident");
+//
+//        given()
+//                .header("Content-Type", "application/json")
+//                .body(requestParams.toJSONString())
+//        .when()
+//                .put("/users/2")
+//        .then()
+//                .body("name", equalTo("morpheus"))
+//                .body("job", equalTo("zion resident"))
+//                .statusCode(200).log().all();
+//    }
+//
+//    @Test(description = "Обновление пользователя методом PATCH")
+//    public void testUpdateUserPATCH() {
+//        baseURI = "https://reqres.in/api";
+//
+//        JSONObject requestParams = new JSONObject();
+//        requestParams.put("name", "morpheus");
+//        requestParams.put("job", "zion resident");
+//
+//        given()
+//                .header("Content-Type", "application/json")
+//                .body(requestParams.toJSONString())
+//        .when()
+//                .patch("/users/2")
+//        .then()
+//                .body("name", equalTo("morpheus"))
+//                .body("job", equalTo("zion resident"))
+//                .statusCode(200).log().all();
+//    }
+//
+//    @Test(description = "Обновление пользователя методом PATCH")
+//    public void testDeleteUser() {
+//        baseURI = "https://reqres.in/api";
+//
+//        delete("/users/2")
+//                .then()
+//                .statusCode(204).log().all();
+//    }
 
 }
